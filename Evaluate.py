c9cf972c755384a4fa4dde8aecbf729a35d1a65e
alchemi5t <soundcrane@gmail.com>
2021-11-27 21:06:43 -0500

    Create Evaluate.py

---
+++ Evaluate.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("python3 datagen.py 5 5 3 2")
+
+os.system("python3 datagen.py 16 16 12 6")
+
+os.system("python3 datagen.py 32 32 32 8")
+
+os.system("python3 datagen.py 64 64 32 12")
+
+os.system("python3 datagen.py 64 64 32 16")
+
+os.system("python3 datagen.py 64 64 100 16")
